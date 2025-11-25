2d1a3d7a73ee8ae46a0005cac7e8f46b0c37a605
Muhammad Zohaib Hassan <64767143+mzh1994@users.noreply.github.com>
2025-11-25 15:24:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,108 @@
 // QUESTIONS (you can replace this with JSON fetch later)
 const questions = [
   {
-    id: 1,
-    text: "What are the prerequisites for deleting business partner attribute master data in SAP BW/4HANA? Note: 2 correct answers.",
-    options: {
-      A: "There must be no transaction data referring to the values.",
-      B: "No hierarchical data must reference the values.",
-      C: "No BW query uses business partner as a free characteristic.",
-      D: "No analysis authorizations reference the values."
+    "id": 1,
+    "text": "What are the prerequisites for deleting business partner attribute master data in SAP BW/4HANA? Note: There are 2 correct answers to this question.",
+    "options": {
+      "A": "There must be no transaction data in a Datastore Object (advanced) referring to business partner values that should be deleted.",
+      "B": "In SAP BW/4HANA, there must be no hierarchical data related to business partner values that should be deleted.",
+      "C": "There must be no BW query as Info der in SAP BW/4HANA that uses business partner as a free characteristic.",
+      "D": "In SAP BW/4HANA, there must be no analysis authorizations related to business partner values that should be deleted."
     },
-    correct: ["A", "B"]
+    "correct": ["A", "B"]
   },
   {
-    id: 2,
-    text: "Which layer in the LSA++ architecture is mainly responsible for virtual data models?",
-    options: {
-      A: "Open ODS Layer",
-      B: "Corporate Memory Layer",
-      C: "Virtualization Layer",
-      D: "Reporting Layer"
+    "id": 2,
+    "text": "What are the main challenges companies face when they want to make data-driven decisions? Note: There are 3 correct answers to this question.",
+    "options": {
+      "A": "Harness the power of fragmented, unstructured data sources to turn them into valuable business insights.",
+      "B": "Uncover the hidden potential in their business by unlocking seamless access to critical insights.",
+      "C": "Boost confidence in the quality of their data.",
+      "D": "Simplify the data landscape to reduce costs and accelerate insights.",
+      "E": "Unlock a new dimension of insights, advanced analytics, and AI capabilities."
     },
-    correct: "C"
+    "correct": ["A", "C", "D"]
+  },
+  {
+    "id": 3,
+    "text": "You create a report with SAP Crystal Reports for Enterprise and need an analysis view as a data source. Which tool can you use to create this analysis view?",
+    "options": {
+      "A": "SAP BusinessObjects Web Intelligence",
+      "B": "SAP Crystal Reports for Enterprise",
+      "C": "SAP Lumira, designer edition",
+      "D": "SAP Analysis for Microsoft Office"
+    },
+    "correct": "C"
+  },
+  {
+    "id": 4,
+    "text": "For which scenarios do you use the SAP HANA model focus? Note: There are 2 correct answers to this question.",
+    "options": {
+      "A": "Define calculations using geospatial functions",
+      "B": "Load snapshots using ABAP CDS Views",
+      "C": "Build views and procedures using SQL script",
+      "D": "Define ABAP Managed Database Procedures in data flows"
+    },
+    "correct": ["A", "C"]
+  },
+  {
+    "id": 5,
+    "text": "Which of the Augmented Analytics Smart Features in SAP Analytics Cloud generates multi-tabbed stories with Overview, Key Influencers, Unexpected Values, and Simulation tabs?",
+    "options": {
+      "A": "Smart Discovery",
+      "B": "Smart Predict",
+      "C": "Search to Insight",
+      "D": "Predictive Forecast"
+    },
+    "correct": "A"
+  },
+  {
+    "id": 6,
+    "text": "How does SAP Business Data Cloud facilitate the use of diverse data sources for AI-powered analytics?",
+    "options": {
+      "A": "By centralizing data from both SAP and non-SAP sources into a unified semantic layer",
+      "B": "By integrating diverse data sources through custom APIs",
+      "C": "By providing a secure platform for storing and managing diverse data sets",
+      "D": "By transforming raw data from diverse urces into a standardized format"
+    },
+    "correct": "A"
+  },
+  {
+    "id": 7,
+    "text": "What does a Composite Provider allow you to do in SAP BW/4HANA? Note: There are 3 correct answers to this question.",
+    "options": {
+      "A": "Create new calculated fields",
+      "B": "Define new restricted key figures",
+      "C": "Combine Info Providers using Joins and Unions",
+      "D": "Integrate SAP HANA calculation views",
+      "E": "Join two ABAP CDS views"
+    },
+    "correct": ["A", "C", "D"]
+  },
+  {
+    "id": 8,
+    "text": "For which requirements do you suggest an SAP HANA modeling focus rather than an SAP BW/4HANA modeling focus? Note: There are 2 correct answers to this question.",
+    "options": {
+      "A": "Leveraging SQL in-house knowledge",
+      "B": "Reporting on a harmonized set of master data",
+      "C": "Loading snapshots or deltas from different sources on a periodic basis",
+      "D": "Finding the best match using a fuzzy search"
+    },
+    "correct": ["A", "D"]
+  },
+  {
+    "id": 9,
+    "text": "Which layer of the layered scalable architecture (LSA++) of SAP BW/4HANA is designed as the main storage for harmonized consistent data?",
+    "options": {
+      "A": "Virtual Data Mart layer",
+      "B": "Open Operational Data Store layer",
+      "C": "Data Acquisition layer",
+      "D": "Flexible Enterprise Data Warehouse Core layer"
+    },
+    "correct": "D"
   }
-];
+]
+;
 
 // STATE
 let mode = null;
@@ -192,3 +272,4 @@ function restart() {
   document.getElementById("result-screen").classList.add("hidden");
   document.getElementById("mode-screen").classList.remove("hidden");
 }
+
